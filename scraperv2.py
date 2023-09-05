5ebcba2773bcd06b333128b33b5629d433798ace
eryk0wski <erykdorywalski@gmail.com>
2023-09-05 15:55:58 +0200

    CSV merged file
    Adding merged csv file and making to easier to input data on the top of the file

--- scraperv2.py
+++ scraperv2.py
@@ -1,8 +1,11 @@
 from playwright.sync_api import sync_playwright
 import time
-from rich import print
 import pandas as pd
 import json
+
+
+pageURL = "https://www.filmweb.pl/films/search"
+iterations = 20
 
 def scrolling():
     def check_json(response):
@@ -10,19 +13,25 @@ def scrolling():
             if "preview" in response.url:
                 #print({"url": response.url, "body": response.json()})
                 json_file = response.json()
+                url = response.url
+                url = int(url.split('/')[-2])
+                
+               
 
                 genres = []
                 for genre in range(len(json_file['genres'])):
                     genres.append(json_file['genres'][genre]['name']['text'])
 
 
-                columns = [json_file['year'], json_file['entityName'],json_file['description']['synopsis'],
-                               json_file['description']['sourceType'],json_file['originalTitle']['title'],genre,json_file['plotOrDescriptionSynopsis']]
+                columns = [url,json_file['year'], json_file['entityName'],json_file['description']['synopsis'],
+                               json_file['description']['sourceType'],json_file['originalTitle']['title'],genres,json_file['plotOrDescriptionSynopsis']]
                 df.loc[len(df)] = columns
             if "rating" in response.url:
                 json_file = response.json()
+                url = response.url
+                url = int(url.split('/')[-2])
 
-                columns = [json_file['count'], json_file['rate'], json_file['countWantToSee']]
+                columns = [url, json_file['count'], json_file['rate'], json_file['countWantToSee']]
                 rating.loc[len(rating)] = columns
                 
         except ValueError:
@@ -32,13 +41,13 @@ def scrolling():
             print('Other error')
 
 
-    def json_to_pandas(json_file, row_number):
+    #def json_to_pandas(json_file, row_number):
         
-        genres = []
-        for genre in range(len(json_file['genres'])):
-            genres.append(json_file['genres'][genre]['name']['text'])
-        df.loc[row_number] = [json_file['year'], json_file['entity_name'],json_file['description']['synopsis'],
-                               json_file['description']['sourceType'],json_file['original_title']['title'],genre,json_file['plotOrDescriptionSynopsis']]
+        #genres = []
+        #for genre in range(len(json_file['genres'])):
+        #    genres.append(json_file['genres'][genre]['name']['text'])
+        #df.loc[row_number] = [json_file['year'], json_file['entity_name'],json_file['description']['synopsis'],
+        #                       json_file['description']['sourceType'],json_file['original_title']['title'],genre,json_file['plotOrDescriptionSynopsis']]
   
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
@@ -46,16 +55,17 @@ def scrolling():
         page.set_viewport_size(
             {"width":1280, "height": 1080}
         )
-        rating = pd.DataFrame(columns = ['count','rating','want_to_see'])
-        df = pd.DataFrame(columns = ['year','entity_name','summary','source_type', 'original_title','genre', 'plot_describtion'])
+        rating = pd.DataFrame(columns = ['url','count','rating','want_to_see'])
+        df = pd.DataFrame(columns = ['url','year','entity_name','summary','source_type', 'original_title','genre', 'plot_describtion'])
         page.on("response", lambda response: check_json(response))
-        page.goto("https://www.filmweb.pl/films/search")
+        #add filmweb url with your searching 
+        page.goto(pageURL)
         time.sleep(2)
         page.click("#didomi-notice-agree-button", timeout=2_000)
 
         page.wait_for_load_state("networkidle")
 
-        for i in range(20):
+        for i in range(iterations):
 
             page.mouse.wheel(0, 7000)
             print("Scrolling")
@@ -69,12 +79,18 @@ def scrolling():
 
         moment=time.strftime("%Y-%b-%d__%H_%M_%S",time.localtime())
 
-        data_file = moment + 'filmweb.csv'
+        data_file = moment  + 'filmweb.csv'
         rating_file = moment + 'ratings.csv'
+        merged_file = moment + 'merged.csv'
 
 
+        df = df.sort_values(by=['url'], ascending = True)
+        rating = rating.sort_values(by=['url'], ascending = True)
+
+        merged = pd.merge(df, rating, on='url')
         df.to_csv(data_file, index=False, encoding='utf-8')
         rating.to_csv(rating_file, index = False, encoding ='utf-8')
+        merged.to_csv(merged_file, index = False, encoding ='utf-8')
 
 
 
